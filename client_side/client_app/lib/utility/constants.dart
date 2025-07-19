da3e5f476871f5a9876d44b23a79caf82bbe96e4
Sandip Paul <paulsandip821@gmail.com>
2025-07-19 15:14:31 +0530

    🛠️ Update

--- client_side/client_app/lib/utility/constants.dart
+++ client_side/client_app/lib/utility/constants.dart
@@ -1,4 +1,4 @@
-//const MAIN_URL = 'http://192.168.1.4:3000';
+//const MAIN_URL = 'http://34.93.143.127:3000';
 const MAIN_URL = 'http://10.0.2.2:3000';//make changes aaccording to your local server----
 
 const FAVORITE_PRODUCT_BOX = 'FAVORITE_PRODUCT_BOX';
